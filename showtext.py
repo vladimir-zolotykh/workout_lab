8e9d2fa0b97e5e143f5912f68e8f3876180b1d56
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-07-31 08:40:57 +0300

    Refactor get_size()
        it1: Iterator[str]

--- showtext.py
+++ showtext.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
+from typing import Iterator
 import itertools
 import tkinter as tk
 import tkinter.simpledialog
 
 
 def get_size(text: str) -> tuple[int, int]:
+    it1: Iterator[str]
+    it2: Iterator[str]
     it1, it2 = itertools.tee(text.split("\n"))
     return len(max(it1, key=len)), sum(1 for _ in it2)
 
