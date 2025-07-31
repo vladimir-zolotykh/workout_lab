cc3ff79d739e9a98e2cf8390cdb3d929a7c83f4a
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-07-31 08:12:38 +0300

    Add get_size() function

--- showtext.py
+++ showtext.py
@@ -5,13 +5,24 @@ import tkinter as tk
 import tkinter.simpledialog
 
 
+def get_size(text: str) -> tuple[int, int]:
+    width: int = 0
+    height: int = 0
+    for line in text.split("\n"):
+        if len(line) > width:
+            width = len(line)
+        height += 1
+    return width, height
+
+
 class ShowText(tkinter.simpledialog.Dialog):
     def __init__(self, *args, text=None, **kwargs):
         self.text = text
         super().__init__(*args, **kwargs)
 
     def body(self, master):
-        text = tk.Text(master, width=50, height=10)
+        w, h = get_size(self.text)
+        text = tk.Text(master, width=w, height=h)
         text.grid()
         text.insert("1.0", self.text)
         return text
