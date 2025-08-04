e3c1c18435dc00997311e054370645dd73c0060d
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2025-08-04 19:46:21 +0300

    Change `pack' to `grid', use tk.X, tk.Y constants

--- scrollableframe.py
+++ scrollableframe.py
@@ -8,17 +8,18 @@ from tkinter import ttk
 class ScrollableFrame(ttk.Frame):
     def __init__(self, container, *args, **kwargs):
         super().__init__(container, *args, **kwargs)
-
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure(0, weight=1)
         canvas = tk.Canvas(self)
-        scrollbar = ttk.Scrollbar(self, orient="vertical", command=canvas.yview)
+        hbar = ttk.Scrollbar(self, orient=tk.HORIZONTAL, command=canvas.xview)
+        vbar = ttk.Scrollbar(self, orient=tk.VERTICAL, command=canvas.yview)
         self.scrollable_frame = ttk.Frame(canvas)
-
         self.scrollable_frame.bind(
-            "<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox("all"))
+            "<Configure>", lambda e: canvas.configure(scrollregion=canvas.bbox(tk.ALL))
         )
-
-        canvas.create_window((0, 0), window=self.scrollable_frame, anchor="nw")
-        canvas.configure(yscrollcommand=scrollbar.set)
-
-        canvas.pack(side="left", fill="both", expand=True)
-        scrollbar.pack(side="right", fill="y")
+        canvas.create_window((0, 0), window=self.scrollable_frame)
+        canvas.configure(yscrollcommand=vbar.set)
+        canvas.configure(xscrollcommand=hbar.set)
+        canvas.grid(column=0, row=0, sticky=tk.NSEW)
+        hbar.grid(column=0, row=1, sticky=tk.EW)
+        vbar.grid(column=1, row=0, sticky=tk.NS)
